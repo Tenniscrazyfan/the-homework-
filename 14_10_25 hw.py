05c209fc0b02a7a4a4ca66e1ef07e6a3d66a1030
Tenniscrazyfan <diyappatil17@gmail.com>
2025-11-15 09:26:34 +0000

    Update 14_10_25 hw.py

--- 14_10_25 hw.py
+++ 14_10_25 hw.py
@@ -1,78 +1,96 @@
-
 import pygame
 import random
-from time import time
+import time
 from pygame.locals import *
 
 pygame.init()
-screen = pygame.display.set_mode((900, 500))
+screen = pygame.display.set_mode((750, 500))
 playing = True
 
-background = pygame.transform.scale(pygame.image.load("images/space bg.png"), (900, 500))
-ship = pygame.transform.scale(pygame.image.load("images/yellow ship.png"), (100, 100))
-
+background = pygame.transform.scale(pygame.image.load("images/space bg.png"), (750, 500))
+ship = pygame.transform.scale(pygame.image.load("images/yellow ship.png"), (90, 85))
+font = pygame.font.SysFont("Times New Roman", 20)
 
 ship_x = 450
 ship_y = 380
 ship_speed = 5
 
-
 bullets = []
-new_bullets = []
-bullet_speed = 5
+bullet_speed = random.randint(1,3)
 bullet_width = 9
 bullet_height = 10
-starttime = time()
+starttime = time.time()
 totaltime = 0
+gameover = False
 
+last_bullet_time = time.time()
 
 while playing:
-    
-    totaltime = time()- starttime
-    print(totaltime)
+    totaltime = time.time() - starttime
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             playing = False
 
-    
         if event.type == pygame.KEYDOWN:
-                if event.key == K_LEFT:
-                    ship_x -= 10
-                if event.key == K_RIGHT:
-                    ship_x += 10 
-    def spawn_bullets():
-        global new_bullets
-        x = random.randint(0, 880)
-        d = pygame.Rect(x, 0, bullet_width, bullet_height)
-        if int(totaltime) % 40 == 0:
+            if event.key == K_LEFT:
+                ship_x -= 10
+            if event.key == K_RIGHT:
+                ship_x += 10
+
+    current_time = time.time()
+
+    if current_time - last_bullet_time >= 1:
+        if totaltime >= 7:  
+            bullet_count = random.randint(2, 4)
+            if totaltime >= 14:
+                bullet_count = random.randint(4,6)
+                if totaltime >= 18:
+                    bullet_count = random.randint(6,9)
+        else:
+            bullet_count = 1
+
+        for i in range(bullet_count):
+            x = random.randint(0, 880)
+            d = pygame.Rect(x, 0, bullet_width, bullet_height)
             bullets.append(d)
-            for b in bullets:
-                pygame.draw.rect(screen, "white", b)
-        
-        # for i in range(random.randint(1,5)):
-        #     for b in bullets:
-        #         b.y += bullet_speed
-            # if b.y < 500:
-            #     new_bullets.append(b)
-            
-    # bullets = new_bullets
-    
 
-    
-    ship_rect = pygame.Rect(ship_x, ship_y, 100, 100)
-    # for b in bullets:
-    #     if ship_rect.colliderect(b):
-    #         print("Game Over!")
-    #         playing = False
+        last_bullet_time = current_time
 
-    
+    ship_rect = pygame.Rect(ship_x, ship_y, 90, 85)
+    for b in bullets:
+        b.y += bullet_speed
+        if ship_rect.colliderect(b):
+            gameover = True
+            break
+
     screen.blit(background, (0, 0))
+
+    for b in bullets:
+        pygame.draw.rect(screen, "white", b)
+
     screen.blit(ship, (ship_x, ship_y))
-    if int(totaltime) % 40 == 0:
-        print("hi")
-        spawn_bullets()
+
+    if gameover == True:
+        screen.fill("black")
+        text1 = font.render("GAME OVER !!!", True, "white")
+        text2 = font.render("Press Q to quit", True, "white")
+
+        screen.blit(text1, (320, 220))
+        screen.blit(text2, (320, 250))
+
         pygame.display.update()
 
+        waiting = True
+        while waiting:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    waiting = False
+                    playing = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == K_q:
+                        waiting = False
+                        playing = False
+
+        break
+
     pygame.display.update()
-
-
